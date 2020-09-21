ce8a591f3065f7271c85bf038c11e8a0f0880369
Tarık Seyceri <tarik@seyceri.info>
2020-09-21 18:17:27 +0300

    Update Windows 10 Update Disabler.bat

--- Windows 10 Update Disabler.bat
+++ Windows 10 Update Disabler.bat
@@ -9,13 +9,17 @@ echo Welcome to Windows 10 Update Disabler
 echo.
 
 rem Check If User Has Admin Privileges
-NET SESSION >nul 2>&1
-if %ERRORLEVEL% EQU 0 (
+timeout /t 1 /nobreak > NUL
+openfiles > NUL 2>&1
+if %errorlevel%==0 (
     echo Running..
 ) else (
     echo You must run me as an Administrator. Exiting..
     echo.
-    pause
+    echo Right-click on me and select ^'Run as Administrator^' and try again.
+    echo.
+    echo Press any key to exit..
+    pause > NUL
     exit
 )
 
@@ -72,12 +76,22 @@ del /s /f /q %Temp%\*.*
 del /s /f /q %AppData%\Temp\*.*
 del /s /f /q %HomePath%\AppData\LocalLow\Temp\*.*
 
+rem Delete Used Drivers Files (Not needed because already installed)
+del /s /f /q %SYSTEMDRIVE%\AMD\*.*
+del /s /f /q %SYSTEMDRIVE%\NVIDIA\*.*
+del /s /f /q %SYSTEMDRIVE%\INTEL\*.*
+
 rem Delete Temporary Folders
 rd /s /q %WinDir%\Temp
 rd /s /q %WinDir%\Prefetch
 rd /s /q %Temp%
 rd /s /q %AppData%\Temp
 rd /s /q %HomePath%\AppData\LocalLow\Temp
+
+rem Delete Used Drivers Folders (Not needed because already installed)
+rd /s /q %SYSTEMDRIVE%\AMD
+rd /s /q %SYSTEMDRIVE%\NVIDIA
+rd /s /q %SYSTEMDRIVE%\INTEL
 
 rem Recreate Empty Temporary Folders
 md %WinDir%\Temp
@@ -88,7 +102,7 @@ md %HomePath%\AppData\LocalLow\Temp
 
 rem Edit Windows.old Folder Privileges and Deleting It
 takeown /F %SYSTEMDRIVE%\Windows.old\* /R /A /D Y
-echo Y | cacls %SYSTEMDRIVE%\Windows.old\*.* /T /grant administrators:F
+echo Y| cacls %SYSTEMDRIVE%\Windows.old\*.* /T /G administrators:F /C
 echo Please wait, this may take a while.
 rmdir /S /Q %SYSTEMDRIVE%\Windows.old\
 
